db843423b520ac8620268dccdfb57f0072ceac95
имя фамилия <Dima>
2024-12-20 21:53:47 +0300

    for experiment

--- src/main/java/pro/sky/shopping/cart/controller/ShoppingCartController.java
+++ src/main/java/pro/sky/shopping/cart/controller/ShoppingCartController.java
@@ -29,16 +29,16 @@ public class ShoppingCartController {
 
     @GetMapping(path = "/hello")
     public String getProduct() {
-        return "Hello World";
+        return "Hello man";
     }
 
     @GetMapping(path = "/bye")
     public String buyProduct() {
-        return "Goodbye World";
+        return "Goodbye man";
     }
 
     @GetMapping(path = "/good afternoon")
     public String goodAfternoon() {
-        return "Goodbye Afternoon";
+        return "Goodbye man";
     }
 }
